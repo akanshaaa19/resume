a238312f179b85e746b415e899dd299df60c72a6
akanshaaa19 <asakhre2002@gmail.com>
2023-01-31 22:07:55 +0530

    fixed link

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -11,7 +11,7 @@ function Socials (props){
    const [dark, setDark] = useState(true)
    return <ul className="socials">
     <a href='https://github.com/akanshaaa19' target="_blank"><li><i className="fa-brands fa-github fa-lg"></i></li></a>
-    <a href='https://www.aedin.com/in/akansha-sakhre-21428b1ba/' target="_blank"><li><i className="fa-brands fa-linkedin fa-lg"></i></li></a>
+    <a href='https://www.linkedin.com/in/akansha-sakhre-21428b1ba/' target="_blank"><li><i className="fa-brands fa-linkedin fa-lg"></i></li></a>
     <a href='mailto: akanshasakhre@gmail.com' target="_blank"><li><i className="fa-solid fa-envelope fa-lg"></i></li></a>
       {/* <button onClick={switchMode} className='light-mode'>{!dark ? <i class="fa-solid fa-sun fa-2x"></i> :<i class="fa-solid fa-moon fa-2x"></i>}</button> */}
    </ul>;
