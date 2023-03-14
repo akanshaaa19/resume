9f3347de38cf1f02e772faa201f33c9124b459ec
akanshaaa19 <asakhre2002@gmail.com>
2023-03-14 14:47:20 +0530

    new projects

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -13,12 +13,13 @@ const projects = [
       "This takes up your details and generates a resume, which you can download in PDF format and share.",
     stack: "ReactJs Context Tailwind",
   },
+
   {
-    index: 8,
+    index: 4,
     projectId: "p3",
-    src: "https://i.ibb.co/MhzYy8V/dashboard.png",
-    github: "https://github.com/akanshaaa19/brikow-assignment",
-    active: "https://akanshaaa19.github.io/zepto-assignment/#/campaign",
+    src: "https://i.ibb.co/qkyg85v/Screenshot-242.png",
+    github: "https://github.com/akanshaaa19/zepto-assignment",
+    active: "https://akanshaaa19.github.io/brikow-assignment/#/Dashboard",
     title: "Dashboard",
     description:
       "Dashboard created for a tech assignment. It uses Redux for some parts of the application.",
@@ -36,38 +37,39 @@ const projects = [
       "A Food ordering app, which uses Redux for state management. It also includes an admin page.",
     stack: "ReactJS Redux Bootstrap",
   },
-
   {
-    index: 4,
+    index: 8,
     projectId: "p3",
-    src: "https://i.ibb.co/qkyg85v/Screenshot-242.png",
-    github: "https://github.com/akanshaaa19/zepto-assignment",
-    active: "https://akanshaaa19.github.io/brikow-assignment/#/Dashboard",
+    src: "https://i.ibb.co/MhzYy8V/dashboard.png",
+    github: "https://github.com/akanshaaa19/brikow-assignment",
+    active: "https://akanshaaa19.github.io/zepto-assignment/#/campaign",
     title: "Dashboard",
     description:
       "Dashboard created for a tech assignment. It uses Redux for some parts of the application.",
     stack: "ReactJS Redux Bootstrap",
   },
-    {
-      index: 2,
-      src: "https://i.ibb.co/BrcKHWh/tasks.png",
-      github: "https://github.com/akanshaaa19/tasks",
-      active: "https://akanshaaa19.github.io/tasks",
-      title: "Tasks App",
-      description: 'You can create tasks, check them off, and then delete them in this app. You can also create different lists.',
-      stack: "ReactJS Context Firebase",
-    },
 
-    {
-      index: 55,
-      src: "https://i.ibb.co/0XJMLsj/weather.png",
-      github: "https://github.com/akanshaaa19/weather-app",
-      active: "https://akanshaaa19.github.io/weather-app",
-      title: "Weather App",
-      description:
-        "This takes a city name as an input, and returns the current temperature, wind, pressure, and humidity etc.",
-      stack: "ReactJS Bootstrap API",
-    },
+  {
+    index: 2,
+    src: "https://i.ibb.co/BrcKHWh/tasks.png",
+    github: "https://github.com/akanshaaa19/tasks",
+    active: "https://akanshaaa19.github.io/tasks",
+    title: "Tasks App",
+    description:
+      "You can create tasks, check them off, and then delete them in this app. You can also create different lists.",
+    stack: "ReactJS Context Firebase",
+  },
+
+  {
+    index: 55,
+    src: "https://i.ibb.co/0XJMLsj/weather.png",
+    github: "https://github.com/akanshaaa19/weather-app",
+    active: "https://akanshaaa19.github.io/weather-app",
+    title: "Weather App",
+    description:
+      "This takes a city name as an input, and returns the current temperature, wind, pressure, and humidity etc.",
+    stack: "ReactJS Bootstrap API",
+  },
   // {
   //     index: 6,
   //     src: "https://i.ibb.co/7j5RqXT/portfolio.png",
